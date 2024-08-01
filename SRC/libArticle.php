a9636df39843930394d7d9b8db9c2b0de121148d
Keiichi Satake <202405ap01_satake@x-ide.sakura.ne.jp>
2024-08-01 14:54:01 +0900

    #25186
    不具合状況　検索の入力値が不正に出力される
    修正内容　検索値のXSS対応、$sRoomと$sArticleの入れ替え、printタグの位置修正

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -6,14 +6,14 @@ function subArticle()
 {
 	$conn = fnDbConnect();
 
-	$sDel         = $_REQUEST['sDel'];
-	$sArticle     = $_REQUEST['sArticle'];
-	$sRoom        = $_REQUEST['sRoom'];
-	$sKeyPlace    = $_REQUEST['sKeyPlace'];
-	$sArticleNote = $_REQUEST['sArticleNote'];
-	$sKeyBox      = $_REQUEST['sKeyBox'];
-	$sDrawing     = $_REQUEST['sDrawing'];
-	$sSellCharge  = $_REQUEST['sSellCharge'];
+	$sDel         = htmlspecialchars($_REQUEST['sDel']);
+	$sArticle     = htmlspecialchars($_REQUEST['sArticle']);
+	$sRoom        = htmlspecialchars($_REQUEST['sRoom']);
+	$sKeyPlace    = htmlspecialchars($_REQUEST['sKeyPlace']);
+	$sArticleNote = htmlspecialchars($_REQUEST['sArticleNote']);
+	$sKeyBox      = htmlspecialchars($_REQUEST['sKeyBox']);
+	$sDrawing     = htmlspecialchars($_REQUEST['sDrawing']);
+	$sSellCharge  = htmlspecialchars($_REQUEST['sSellCharge']);
 
 	$orderBy = $_REQUEST['orderBy'];
 	$orderTo = $_REQUEST['orderTo'];
@@ -58,19 +58,19 @@ function subArticle()
 				</tr>
 				<tr>
 					<th>物件名</th>
-					<td><input type="text" name="sArticle" value="<?php print $sRooms ?>" size="50" /></td>
+					<td><input type="text" name="sArticle" value="<?php print $sArticle ?>" size="50" /></td>
 					<th>キーBox番号</th>
 					<td><input type="text" name="sKeyBox" value="<?php print $sKeyBox ?>" size="30" /></td>
 				</tr>
 				<tr>
 					<th>部屋番号</th>
-					<td><input type="text" name="sRoom" value="" size="30" /><?php print $sArticle ?></td>
+					<td><input type="text" name="sRoom" value="<?php print $sRoom ?>" size="30" /></td>
 					<th>3Dパース</th>
 					<td><input type="text" name="sDrawing" value="<?php print $sDrawing ?>" size="30" /></td>
 				</tr>
 				<tr>
 					<th>鍵場所</th>
-					<td><input type="text" name="sKeyPlace" value="<?php print $sKagPlace ?>" size="30" /></td>
+					<td><input type="text" name="sKeyPlace" value="<?php print $sKeyPlace ?>" size="30" /></td>
 					<th>営業担当者</th>
 					<td><input type="text" name="sSellCharge" value="<?php print $sSellCharge ?>" /></td>
 				</tr>
@@ -112,14 +112,14 @@ function subArticle()
 				$res = mysqli_query($conn, $sql);
 				$i = 0;
 				while ($row = mysqli_fetch_array($res)) {
-					$articleNo   = $row["ARTICLENO"];
-					$article     = $row["ARTICLE"];
-					$room        = $row["ROOM"];
-					$keyPlace    = $row["KEYPLACE"];
-					$articleNote =  $row["ARTICLENOTE"];
-					$keyBox      = $row["KEYBOX"];
-					$drawing     = $row["DRAWING"];
-					$sellCharge  = $row["SELLCHARGE"];
+					$articleNo   = htmlspecialchars($row["ARTICLENO"]);
+					$article     = htmlspecialchars($row["ARTICLE"]);
+					$room        = htmlspecialchars($row["ROOM"]);
+					$keyPlace    = htmlspecialchars($row["KEYPLACE"]);
+					$articleNote = htmlspecialchars($row["ARTICLENOTE"]);
+					$keyBox      = htmlspecialchars($row["KEYBOX"]);
+					$drawing     = htmlspecialchars($row["DRAWING"]);
+					$sellCharge  = htmlspecialchars($row["SELLCHARGE"]);
 				?>
 					<tr>
 						<td class="list_td<?php print $i ?>"><a href="javascript:form.act.value='articleEdit';form.articleNo.value=<?php print $articleNo ?>;form.submit();"><?php print $article ?></a></td>
